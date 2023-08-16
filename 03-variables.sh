9456c2a52049c633f6e78d13e8fdfd895c20a32d
Chandu154 <devarachandra4@gmail.com>
2023-08-16 17:18:44 +0530

    variable date

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 echo "hello world , i am learning devops with aws"
+PERSON1=$1 # this is first value from command line 
 PERSON2=$2 # this is second value from command line 
 echo "$PERSON1: Hi $PERSON2 ,good morning"
 echo "$PERSON2: good morning $PERSON1"
