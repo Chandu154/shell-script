70331f13760c50e873eeda1a6f15c6cca2e13c38
Chandu154 <devarachandra4@gmail.com>
2023-08-16 17:56:32 +0530

    variable date

--- 05-variable.sh
+++ 05-variable.sh
@@ -4,3 +4,10 @@ echo "please enter username"
 read USERNAME # this is the variable where the value of  one user enterd
 
 echo "user entered is:$USERNAME"
+
+echo "please enter password"
+
+read PASSWORD 
+ echo "password entered is:$PASSWORD"
+
+
