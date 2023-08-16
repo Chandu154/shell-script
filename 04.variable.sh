e3499f701d363992aa7039f3ca9fac5fa0572be7
Chandu154 <devarachandra4@gmail.com>
2023-08-16 17:27:21 +0530

    variable date

---
+++ 04.variable.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+#we are adding the two numbhers with variable
+
+NUMBER1=$1
+NUMBER2=$2
+# i need to run this command inside the shell to add thses 2  numbers
+SUM=$((NUMBER1+NUMBER2))
