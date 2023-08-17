aba84cf4d83154ae5ae13d167e5f996eb311844f
Chandu154 <devarachandra4@gmail.com>
2023-08-17 15:12:11 +0530

    variable date

--- 09-Functions.sh
+++ 09-Functions.sh
@@ -18,10 +18,10 @@ VALIDATE () {
     #$1 it will receive the arguments.
     if [ $1 -ne 0 ]
   then 
-     echo "$2... $R Failure $N"
+     echo -e "$2... $R Failure $N"
      exit 1
   else 
-    echo "$2... $G success $N"
+    echo -e "$2... $G success $N"
 fi   
 }
 
