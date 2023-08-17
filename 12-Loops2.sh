95ba426d88c07b01e23089f106d141894eef3eca
Chandu154 <devarachandra4@gmail.com>
2023-08-17 15:47:37 +0530

    variable date

--- 12-Loops2.sh
+++ 12-Loops2.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+## all perametrs or arguments will come and store in $@ and install one by one 
+
+
+DATE=$(date +%F)
+
+SCRIPT_NAME=$0
+
+LOGFILE=/dev/$SCRIPT_NAME-$DATE.log
+
 
 for i in $@
  do 
-  yum install $i -y
+  yum install $i -y &>>$LOGFILE 
  done 
