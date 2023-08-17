952b6d7519236c9e9f2b200f906009ad97473e68
Chandu154 <devarachandra4@gmail.com>
2023-08-17 10:34:53 +0530

    variable date

---
+++ 07-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## writing the condition to check the number is greater or less then the 10 
+
+NUMBER=$1
+
+if [$NUMBER -gt 10]
+ then 
+  echo "$NUMBER is greater then 10"
+ else
+  echo "$NUMBER is less then 10" 
+fi  
