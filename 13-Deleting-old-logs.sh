eb8f85d9809e4a328448dd4e1bcfe76c008e7cef
Chandu154 <devarachandra4@gmail.com>
2023-08-22 10:37:17 +0530

    variable date

---
+++ 13-Deleting-old-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F)
+
+SCRIPT_NAME=$0
+LOGSDIR=/home/centos/shellscript.logs
+#/home/centos/shellscript-logs-scriptname-date.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
+
+FILES_TO_DELETE= $(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
+
