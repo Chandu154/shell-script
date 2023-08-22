511d0a936d62750f3084a0bb07d5b5bb2101f4fd
Chandu154 <devarachandra4@gmail.com>
2023-08-22 17:44:22 +0530

    variable date

--- 13-Deleting-old-logs.sh
+++ 13-Deleting-old-logs.sh
@@ -11,11 +11,10 @@ LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "script started executing at $DATE" &>> $LOGFILE
+echo "script started executing at $DATE" &>>$LOGFILE
 
 while read line 
 do
-  echo "Deleting $line" &>> $LOGFILE 
+  echo "Deleting $line" &>>$LOGFILE 
   rm -rf $line 
-    
 done <<< $FILES_TO_DELETE
