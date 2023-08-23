6a74f80b41bca1bfd8f2c8bd876eb2ed82869794
Chandu154 <devarachandra4@gmail.com>
2023-08-23 16:39:56 +0530

    variable date

--- mail.sh
+++ mail.sh
@@ -15,4 +15,5 @@ ALERT_TYPE=$5
 #echo "all args: $@"
 FINAL_BODY=$(sed -e "s/Team/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html )
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
